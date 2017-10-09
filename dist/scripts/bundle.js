78427622047fc72f8bb5e14637cdf256c7641473
viniciobb <viniciobb@gmail.com>
2017-10-09 20:43:28 -0300

    transitions locations and redirects

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -49169,6 +49169,26 @@ module.exports = {
 "use strict";
 var React = require('react');
 var About = React.createClass({displayName: "About",
+    statics: {
+        willTransitionTo: function(transition, params, query, callback){
+            if(!confirm('Are you sure you read a page that\'s this boring ?')){
+                transition.about();
+
+            }else{
+                
+                callback();
+            }
+
+        },
+
+        willTransitionFrom: function(transition, component){
+            if(!confirm('Are you sure you lead a page that\'s so exciting ??')){
+                transition.about();
+            }    
+        }
+
+    },
+    
     render: function(){
         return (
 
@@ -49303,6 +49323,9 @@ module.exports = AuthorsPage;
 },{"../../api/authorApi":198,"./authorList":202,"react":197}],204:[function(require,module,exports){
 "use strict";
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
+
 var Header = React.createClass({displayName: "Header",
     render: function(){
         return (
@@ -49310,9 +49333,9 @@ var Header = React.createClass({displayName: "Header",
             React.createElement("nav", {className: "navbar navbar-default"}, 
                 React.createElement("div", {className: "container-fluid"}, 
                     React.createElement("ul", {className: "nav navbar-nav"}, 
-                        React.createElement("li", null, React.createElement("a", {href: "/"}, "Home")), 
-                        React.createElement("li", null, React.createElement("a", {href: "/#authors"}, "Authors")), 
-                        React.createElement("li", null, React.createElement("a", {href: "/#about"}, "About"))
+                        React.createElement("li", null, React.createElement(Link, {to: "app"}, "Home")), 
+                        React.createElement("li", null, React.createElement(Link, {to: "authors"}, "Authors")), 
+                        React.createElement("li", null, React.createElement(Link, {to: "about"}, "About"))
                     )
                 )
             )
@@ -49324,18 +49347,20 @@ var Header = React.createClass({displayName: "Header",
 
 module.exports = Header; 
 
-},{"react":197}],205:[function(require,module,exports){
+},{"react":197,"react-router":28}],205:[function(require,module,exports){
 "use strict";
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
+
 var Home = React.createClass({displayName: "Home",
     render: function(){
         return (
-
             React.createElement("div", {className: "jumbotron"}, 
                 React.createElement("h1", null, "Pluralsight administration"), 
-                React.createElement("p", null, " React, React Router and Flux for ultra responsive web apps.")
+                React.createElement("p", null, " React, React Router and Flux for ultra responsive web apps."), 
+                React.createElement(Link, {to: "about", className: "btn btn-primary btn-lg"}, "Learn more")
             )
-
         );
     }
 
@@ -49343,7 +49368,26 @@ var Home = React.createClass({displayName: "Home",
 
 module.exports = Home; 
 
-},{"react":197}],206:[function(require,module,exports){
+},{"react":197,"react-router":28}],206:[function(require,module,exports){
+"use strict";
+var React = require('react');
+var Link = require('react-router').Link;
+
+var NotFoundPage = React.createClass({displayName: "NotFoundPage",
+    render: function(){
+        return (
+            React.createElement("div", null, 
+                React.createElement("h1", null, "Page not found"), 
+                React.createElement("p", null, "Whoops! Sorry, there is nothing to see here."), 
+                React.createElement("p", null, React.createElement(Link, {to: "app"}, "Back to Home"))
+            )    
+        );
+    }
+});
+
+module.exports = NotFoundPage; 
+
+},{"react":197,"react-router":28}],207:[function(require,module,exports){
 "use strict";
 var React = require('react');
 var Router = require('react-router');
@@ -49353,21 +49397,28 @@ Router.run(routes, function(Handler){
     React.render(React.createElement(Handler, null), document.getElementById('app'));
 });
 
-},{"./routes":207,"react":197,"react-router":28}],207:[function(require,module,exports){
+},{"./routes":208,"react":197,"react-router":28}],208:[function(require,module,exports){
 "use strict";
 var React = require('react');
 var Router = require("react-router");
 var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
+var Redirect = Router.Redirect;
+
 
 var routes = (
     React.createElement(Route, {name: "app", path: "/", handler: require('./components/app')}, 
         React.createElement(DefaultRoute, {handler: require('./components/homePage')}), 
         React.createElement(Route, {name: "authors", handler: require('./components/authors/authorPage')}), 
-        React.createElement(Route, {name: "about", handler: require('./components/about/aboutPage')})
+        React.createElement(Route, {name: "about", handler: require('./components/about/aboutPage')}), 
+        React.createElement(NotFoundRoute, {handler: require('./components/notFoundPage')}), 
+        React.createElement(Redirect, {from: "about-us", to: "about"}), 
+        React.createElement(Redirect, {from: "awthors", to: "authors"}), 
+        React.createElement(Redirect, {from: "about*", to: "about"})
     )
 );
 
 module.exports = routes;
 
-},{"./components/about/aboutPage":200,"./components/app":201,"./components/authors/authorPage":203,"./components/homePage":205,"react":197,"react-router":28}]},{},[206]);
+},{"./components/about/aboutPage":200,"./components/app":201,"./components/authors/authorPage":203,"./components/homePage":205,"./components/notFoundPage":206,"react":197,"react-router":28}]},{},[207]);
--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,5 +1,8 @@
 "use strict";
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
+
 var Header = React.createClass({
     render: function(){
         return (
@@ -7,9 +10,9 @@ var Header = React.createClass({
             <nav className="navbar navbar-default">
                 <div className="container-fluid">
                     <ul className="nav navbar-nav">
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/#authors">Authors</a></li>
-                        <li><a href="/#about">About</a></li>
+                        <li><Link to="app">Home</Link></li>
+                        <li><Link to="authors">Authors</Link></li>
+                        <li><Link to="about">About</Link></li>
                     </ul>
                 </div>
             </nav>
